8e8a436e9e2e4475d11b19f3f48a62107b70005d
prakashtaz0091 <code.ing2468@gmail.com>
2024-03-11 00:01:44 +0545

    type safety with ts

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,11 +1,19 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Colors from '../constants/Colors'
+import { Product } from '@/assets/types'
 
-export default function ProductItem({product}) {
+export const defaultImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+
+type ProductItemProps = {
+    product: Product;
+}
+
+
+export default function ProductItem({product}:ProductItemProps) {
   return (
     <View style={styles.container}>
-        <Image source={{uri:product.image}} style={styles.image} />
+        <Image source={{uri:product.image||defaultImage}} style={styles.image} />
         <Text style={styles.title} >{product&&product.name}</Text>
         <Text style={styles.price} >${product&&product.price}</Text>
       </View>
